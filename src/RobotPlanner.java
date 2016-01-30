bc5f4d07b251189704052376b9b8f67566060bb7
dokutagero <juanjo.rubio@est.fib.upc.edu>
2016-01-30 17:08:04 +0100

    working project

--- src/RobotPlanner.java
+++ src/RobotPlanner.java
@@ -10,7 +10,7 @@ public class RobotPlanner {
 
     public static void main(String[] args){
 
-        String filename = new String("settings4");
+        String filename = new String("settings5");
 
         //Parse initial files and obtain the board configuration.
         PARser parser = new PARser();
