d1814bfeac6db5feaa66375144a6a61eb269a196
dokutagero <juanjo.rubio@est.fib.upc.edu>
2016-01-31 15:56:19 +0100

    Added new print of stack

--- src/RobotPlanner.java
+++ src/RobotPlanner.java
@@ -10,7 +10,7 @@ public class RobotPlanner {
 
     public static void main(String[] args){
 
-        String filename = new String("settings");
+        String filename = new String("settings2");
 
         //Parse initial files and obtain the board configuration.
         PARser parser = new PARser();
